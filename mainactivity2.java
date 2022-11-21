3755d69968b80e5ec8c748d043b4c73b41b36e70
Mousamshivhare <90505098+Mousamshivhare@users.noreply.github.com>
2022-11-21 19:43:39 +0530

    Create mainactivity2.java

---
+++ mainactivity2.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    Intent intent = getIntent();
+    String message = intent.getStringExtra(MainActivity.MSG);
+    TextView textView = findViewById(R.id.textView2);
+    textView.setText(message);
